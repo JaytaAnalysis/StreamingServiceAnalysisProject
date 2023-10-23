f25893eb091e5ee4959d7e3582e5d7c6ad63ca2c
JaytaAnalysis <146982612+JaytaAnalysis@users.noreply.github.com>
2023-10-23 17:21:47 -0600

    Update DirectorPrevalence.sql
    Update Description

--- DirectorPrevalence.sql
+++ DirectorPrevalence.sql
@@ -1,10 +1,19 @@
+/*
+This query selects the unique directors' names, counts how many movies and tv shows each has directed, and then orders them by the number
+of directors in descending order.
+This query can also produce the same list but for actors instead of directors by replacing "director" with "actor".
+The purpose of this query is to see what trends there are in the directors and actors of movies and shows. For instance, it could be useful
+to know if directors/actors tend to only make movies or shows but not both.
+*/
 SELECT 
   COUNT(directors) AS num_directors, 
   directors,
   COUNTIF(duration_type = 'Seasons') AS num_tv,
   COUNTIF(duration_type = 'min') AS num_movie
 FROM `asymmetric-moon-377217.streaming_libraries.netflix_library`, UNNEST(SPLIT(director,',')) AS directors
-/* Ignore any titles where the table does not indicate if it is a movie or a tv show */
+/* 
+Ignore any titles where the table does not indicate if it is a movie or a tv show
+*/
 WHERE duration_type IS NOT NULL
 GROUP BY directors
 ORDER BY num_directors DESC
