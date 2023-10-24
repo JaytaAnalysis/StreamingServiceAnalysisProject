71d518254fa61e70aafd23cb4463b18278a85e3d
JaytaAnalysis <146982612+JaytaAnalysis@users.noreply.github.com>
2023-10-23 22:37:37 -0600

    Update NumberOfMoviesOverTime.sql

--- NumberOfMoviesOverTime.sql
+++ NumberOfMoviesOverTime.sql
@@ -1,5 +1,6 @@
 /* 
-The goal of this query is to create a table showing the number of release dates in each month throughout the years
+The goal of this query is to create a table showing the number of movie release dates in each month over time. This is useful because
+showing that certain months are more popular can influence when to release a new movie.
 */
 SELECT
   COUNT(*) AS num_titles,
@@ -7,19 +8,15 @@ SELECT
   EXTRACT(YEAR FROM DATE_TRUNC(PARSE_DATE('%b %Y', my_table.my_str),MONTH)) AS year_name
 FROM
 /*
-Use a subquery to create a smaller table that includes only the month and year of the date added, since I am not going to use the day information in my analysis. Use string functions like STRPOS and SUBSTR to extract the month/year data based on knowing the format of the date as "month day, year". Add a WHERE clause to eliminate any NA values before creating the table.
-
-I will also include the various libraries that you can perform this query on, since it should work for each streaming service's library as well as the cumulative library.
-
-`asymmetric-moon-377217.streaming_libraries.disney_library`
-`asymmetric-moon-377217.streaming_libraries.hulu_library`
-`asymmetric-moon-377217.streaming_libraries.netflix_library`
-`asymmetric-moon-377217.streaming_libraries.prime_library`
-`asymmetric-moon-377217.streaming_libraries.full_library`
+Use a subquery to create a smaller table that includes only the month and year of the date added, since I am not going to use the day 
+information in my analysis. Use string functions like STRPOS and SUBSTR to extract the month/year data based on knowing the format of the 
+date as "month day, year". Add a WHERE clause to eliminate any NA values before creating the table.
 */
-
-  (SELECT CONCAT(SUBSTR(date_added,1,STRPOS(date_added," ")-1),SUBSTR(date_added,LENGTH(date_added)-4,LENGTH(date_added))) AS my_str
-  FROM `asymmetric-moon-377217.streaming_libraries.disney_library` WHERE date_added IS NOT NULL) AS my_table
+  (
+  SELECT CONCAT(SUBSTR(date_added,1,STRPOS(date_added," ")-1),SUBSTR(date_added,LENGTH(date_added)-4,LENGTH(date_added))) AS my_str
+  FROM `asymmetric-moon-377217.streaming_libraries.disney_library` 
+  WHERE date_added IS NOT NULL
+  ) AS my_table
 GROUP BY
   year_name, month_name
 ORDER BY
